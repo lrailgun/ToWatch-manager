937cb088f3f37e30ffa6b05a12dd86bd2c6cf5d2
lrailgun <lrailgun667@gmail.com>
2025-10-22 12:40:01 +0300

    Fix paths to support execution via symlink

--- watchlist.sh
+++ watchlist.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-shopt -s nocasematch # Making script non case-sensitive
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+shopt -s nocasematch
+SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)"
 
 # importing files
-source $SCRIPT_DIR/watchlist_functions.sh
+source "$SCRIPT_DIR/watchlist_functions.sh"
 animelist="$SCRIPT_DIR/anime.txt"
 movielist="$SCRIPT_DIR/movies.txt"
 serieslist="$SCRIPT_DIR/series.txt"
@@ -11,46 +11,49 @@ cartoonlist="$SCRIPT_DIR/cartoons.txt"
 videolist="$SCRIPT_DIR/videos.txt"
 
 # Associating files
-mapfile -t anime < $animelist
-mapfile -t movie < $movielist
-mapfile -t series < $serieslist
-mapfile -t cartoon < $cartoonlist
-mapfile -t video < $videolist
+mapfile -t anime < "$animelist"
+mapfile -t movie < "$movielist"
+mapfile -t series < "$serieslist"
+mapfile -t cartoon < "$cartoonlist"
+mapfile -t video < "$videolist"
 
-# Announcing menus
-
-
-#Start
+# Start
 echo "Welcome to ToWatch-manager."
 echo "What's on your mind?"
 menu_1
+
 case $menu1_choice in
-    "1") # Defining what to add
+    "1")
         echo "What do you want to add?"
         menu_2
         set_type
-        arrayname="$type" # Setting correct array
-        filevar="${type}list" # Setting correct file
-        add_item;;
-    "2") # Defining what to remove
+        arrayname="$type"
+        filevar="${type}list"
+        add_item
+        ;;
+    "2")
         echo "What do you want to remove?"
         menu_2
-        read menu2_choice
         set_type
-        arrayname="$type" # Setting correct array
-        filevar="${type}list" # Setting correct file
-        remove_item;;
-    "3") # Defining what to browse
+        arrayname="$type"
+        filevar="${type}list"
+        remove_item
+        ;;
+    "3")
         echo "What are you looking for?"
         menu_2
-        read menu2_choice
         set_type
-        arrayname="$type" # Setting correct array
-        filevar="${type}list" # Setting correct file
+        arrayname="$type"
+        filevar="${type}list"
         echo "Your $type list:"
         eval "for i in \"\${!$arrayname[@]}\"; do
             echo \"\$((i+1))) \${$arrayname[i]}\"
-        done";;
-    "0") echo; echo "See you next time!"; sleep 0.5; echo "exiting..."; sleep 0.5; exit 0;;
-    *) echo; echo "Incorrect input"; sleep 0.5; echo "exiting..."; sleep 0.5; exit 1;;
+        done"
+        ;;
+    "0")
+        echo "See you next time!"; exit 0
+        ;;
+    *)
+        echo "Incorrect input. Exiting..."; exit 1
+        ;;
 esac
--- watchlist_functions.sh
+++ watchlist_functions.sh
@@ -2,17 +2,17 @@
 shopt -s nocasematch
 
 menu_1() {
-echo "
+    echo "
 1. Add new title
 2. Remove existing title
 3. Browse lists
 0. Exit
 "
-read -p "Your choise: " menu1_choice
+    read -p "Your choice: " menu1_choice
 }
 
 menu_2() {
-echo "
+    echo "
 1. Show anime list
 2. Show movies list
 3. Show series list
@@ -20,7 +20,7 @@ echo "
 5. Show videos list
 0. Exit
 "
-read -p "Your choice: " menu2_choice
+    read -p "Your choice: " menu2_choice
 }
 
 add_item() {
@@ -29,23 +29,20 @@ add_item() {
         echo \"\$((i+1))) \${$arrayname[i]}\"
     done"
     echo
-    echo "Alright! Let's add some new $type to your list"
     read -p "Enter the title name: " title_name
     read -p "Enter the URL: " title_url
-    read -p "Are you sure you wanna add $title_name | $title_url to your $type list? (Y/n): " add_confirm
+    read -p "Add '$title_name | $title_url' to your $type list? (Y/n): " add_confirm
     add_confirm=${add_confirm:-Y}
 
     if [[ $add_confirm =~ ^[Yy]$ ]]; then
         eval "$arrayname+=(\"\$title_name | \$title_url\")"
         eval "printf '%s\n' \"\${$arrayname[@]}\" > \"\$$filevar\""
-        echo "Title $title_name was successfully added to your $type list!"
-        exit 0;
-
-        elif [[ $add_confirm =~ ^[Nn]$ ]]; then
-            echo "You changed your mind. Exiting..."; exit 0
-
-        else
-            echo "incorrect input. Exiting..." exit 1
+        echo "Title '$title_name' was successfully added to your $type list!"
+        exit 0
+    elif [[ $add_confirm =~ ^[Nn]$ ]]; then
+        echo "Cancelled. Exiting..."; exit 0
+    else
+        echo "Incorrect input. Exiting..."; exit 1
     fi
 }
 
@@ -55,25 +52,22 @@ remove_item() {
         echo \"\$((i+1))) \${$arrayname[i]}\"
     done"
     echo
-    echo "Alright! Let's remove some $type"
-    read -p "Which $type ID you want to remove from the list?: " remove_id
+    read -p "Which $type ID do you want to remove?: " remove_id
     eval "target_item=\${$arrayname[\$((remove_id-1))]}"
-    read -p "Are you sure you want to remove '$target_item' from your $type list? (Y/n): " remove_confirm
+    read -p "Remove '$target_item' from your $type list? (Y/n): " remove_confirm
     remove_confirm=${remove_confirm:-Y}
 
-        if [[ $remove_confirm =~ ^[Yy]$ ]]; then
-            eval "removed_title=\${$arrayname[\$((remove_id-1))]}"
-            eval "unset '$arrayname[\$((remove_id-1))]'"
-            eval "printf '%s\n' \"\${$arrayname[@]}\" > \"\$$filevar\""
-            echo "Title $removed_title was successfully removed from your $type list!"
-            exit 0
-
-            elif [[ $remove_confirm =~ ^[Nn]$  ]]; then
-                    echo "You changed your mind. Exiting..."; exit 0
-
-            else
-                echo "incorrect input. Exiting..."; exit 0
-        fi
+    if [[ $remove_confirm =~ ^[Yy]$ ]]; then
+        eval "removed_title=\${$arrayname[\$((remove_id-1))]}"
+        eval "unset '$arrayname[\$((remove_id-1))]'"
+        eval "printf '%s\n' \"\${$arrayname[@]}\" > \"\$$filevar\""
+        echo "Title '$removed_title' was successfully removed from your $type list!"
+        exit 0
+    elif [[ $remove_confirm =~ ^[Nn]$ ]]; then
+        echo "Cancelled. Exiting..."; exit 0
+    else
+        echo "Incorrect input. Exiting..."; exit 1
+    fi
 }
 
 set_type() {
@@ -83,7 +77,7 @@ set_type() {
         "3") type="series";;
         "4") type="cartoon";;
         "5") type="video";;
-        "0") echo; echo "See you next time!"; sleep 0.5; echo "exiting..."; sleep 0.5; exit 0;;
-        *) echo; echo "Incorrect input"; sleep 0.5; echo "exiting..."; sleep 0.5; exit 1;;
+        "0") echo "See you next time!"; exit 0;;
+        *) echo "Incorrect input. Exiting..."; exit 1;;
     esac
 }
